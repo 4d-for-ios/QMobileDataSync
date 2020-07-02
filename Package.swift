3a232978c4d9aa0c804c8ddf799e33f19d1c5b69
Eric Marchand <59135882+mesopelagique@users.noreply.github.com>
2020-07-02 11:50:49 +0200

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "QMobileDataSync",
+    platforms: [
+        .macOS(.v10_14),
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "QMobileDataSync",
+            targets: ["QMobileDataSync"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/DaveWoodCom/XCGLogger.git", from: "7.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "QMobileDataSync",
+            dependencies: ["XCGLogger"],
+            path: "Sources")
+    ]
+)
